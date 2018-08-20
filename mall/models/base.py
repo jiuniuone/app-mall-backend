3833640f4218d2c6b2051ee7c6cdd02283769462
hikelee <hikelee@gmail.com>
2018-08-20 13:35:15 +0800

    update

--- mall/models/base.py
+++ mall/models/base.py
@@ -6,3 +6,6 @@ from acmin.models import AcminModel
 class Base(AcminModel, models.Model):
     class Meta:
         abstract = True
+
+    created = models.DateTimeField('创建时间', auto_now_add=True, null=True)
+    modified = models.DateTimeField('更新时间', auto_now=True)
